c857500dcae6424cf960f6c7e5b7ac60a607953e
Ernesto Rapetti <erapetti@yahoo.com>
2019-07-11 00:50:33 -0300

    Uso la fecha en los archivos incluidos en producción

--- tasks/config/sails-linker.js
+++ tasks/config/sails-linker.js
@@ -57,7 +57,7 @@ module.exports = function(grunt) {
       options: {
         startTag: '<!--SCRIPTS-->',
         endTag: '<!--SCRIPTS END-->',
-        fileTmpl: '<script src="/node/CompensacionElecciones%s"></script>',
+        fileTmpl: '<script src="/node/CompensacionElecciones%s?v=<%- (new Date()).getTime() %>"></script>',
         appRoot: '.tmp/public'
       },
       files: {
@@ -128,7 +128,7 @@ module.exports = function(grunt) {
       options: {
         startTag: '<!--STYLES-->',
         endTag: '<!--STYLES END-->',
-        fileTmpl: '<link rel="stylesheet" href="/node/CompensacionElecciones%s">',
+        fileTmpl: '<link rel="stylesheet" href="/node/CompensacionElecciones%s?v=<%- (new Date()).getTime() %>">',
         appRoot: '.tmp/public'
       },
       files: {
