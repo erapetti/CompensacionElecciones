16ccddae110c8c36705e95a2bb14014aa441c819
erapetti <erapetti@yahoo.com>
2019-07-18 19:11:21 -0300

    Agrego grant en Personas

--- grants.sql
+++ grants.sql
@@ -1,4 +1,6 @@
 CREATE USER 'compelec'@'localhost' identified by 'compelec';
 GRANT SELECT ON `Direcciones`.* TO 'compelec'@'localhost';
 GRANT INSERT ON Personal.COMPELECPERIODOSOPCIONES TO 'compelec'@'localhost';
-GRANT SELECT ON Personal.* TO compelec'@'localhost';
+GRANT SELECT ON Personal.* TO 'compelec'@'localhost';
+GRANT SELECT ON Personas.* TO 'compelec'@'localhost';
+
